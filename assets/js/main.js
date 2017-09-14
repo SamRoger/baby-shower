901c1f78226222b16ebecae2c2b0da66cec195d3
Sam Almeida <samRAlmeida@icloud.com>
2017-09-14 15:19:12 -0400

    iphone and initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,20 @@
-// var background1 = document.getElementsByClassName("background1")[0];
-// var pumpkin = document.getElementsByClassName("pumpkin-logo")[0];
+var background1 = document.getElementsByClassName("background1")[0];
+var background2 = document.getElementsByClassName("background2")[0];
+var pumpkin = document.getElementsByClassName("pumpkin-logo")[0];
+var theWay = document.getElementsByClassName("the-way")[0];
 
-// window.onload = function(){
-// 	background1.style.animation = "cardFadeIn 2s forwards";
-// }
+window.onload = function(){
+	background1.style.animation = "cardFadeIn 2s forwards";
+}
+
+setTimeout(function(){
+	background2.style.animation = "cardFadeIn 2s forwards";
+},750)
+setTimeout(function(){
+	pumpkin.style.animation = "topDown 1s forwards";
+	pumpkin.style.opacity = "1.5";
+},1500)
+setTimeout(function(){
+	theWay.style.animation = "leftRight 1.5s forwards";
+	pumpkin.style.opacity = "1.5";
+},2000)
