0064f23eaac21e46b672281adc3e481dc6a2c97d
Sam Almeida <samRAlmeida@icloud.com>
2017-09-14 20:07:04 -0400

    iphone and initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,19 +2,19 @@ var background1 = document.getElementsByClassName("background1")[0];
 var background2 = document.getElementsByClassName("background2")[0];
 var pumpkin = document.getElementsByClassName("pumpkin-logo")[0];
 var theWay = document.getElementsByClassName("the-way")[0];
+var mediaAmazon2 = document.getElementsByClassName("mediaAmazon2")[0];
 
 window.onload = function(){
-	background1.style.animation = "cardFadeIn 2s forwards";
+	background1.style.animation = "cardFadeIn 1.5s forwards";
 }
 
 setTimeout(function(){
-	background2.style.animation = "cardFadeIn 2s forwards";
-},750)
-setTimeout(function(){
-	pumpkin.style.animation = "topDown 1s forwards";
-	pumpkin.style.opacity = "1.5";
-},1500)
-setTimeout(function(){
-	theWay.style.animation = "leftRight 1.5s forwards";
-	pumpkin.style.opacity = "1.5";
-},2000)
+	background2.style.animation = "cardFadeIn 1.5s forwards";
+},800)
+
+var tapHere = "<div class='tapHere'>" + 'Tap Here For Our Registry' + "</div>";
+
+
+mediaAmazon2.innerHTML = tapHere;
+
+
