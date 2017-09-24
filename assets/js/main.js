a18f7da7cec03e1741bce3e5d44406b3bf8fa861
Sam Almeida <samRAlmeida@icloud.com>
2017-09-24 16:06:33 -0400

    ie remove

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,28 +57,21 @@ experience.addEventListener("click", function(){
 	console.log("hi")
 })
 
+function msieversion() 
+{
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
 
-
-function Check_Version(){
-    var rv = -1; // Return value assumes failure.
-
-    if (navigator.appName == 'Microsoft Internet Explorer'){
-
-       var ua = navigator.userAgent,
-           re  = new RegExp("MSIE ([0-9]{1,}[\\.0-9]{0,})");
-
-       if (re.exec(ua) !== null){
-         rv = parseFloat( RegExp.$1 );
-       }
+    if (msie > 0) // If Internet Explorer, return version number
+    {
+        alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
     }
-    else if(navigator.appName == "Netscape"){                       
-       /// in IE 11 the navigator.appVersion says 'trident'
-       /// in Edge the navigator.appVersion does not say trident
-       if(navigator.appVersion.indexOf('Trident') === -1) rv = 12;
-       else rv = 11;
-    }       
+    else  // If another browser, return 0
+    {
+        alert('otherbrowser');
+    }
 
-    return rv;          
+    return false;
 }
 
 
